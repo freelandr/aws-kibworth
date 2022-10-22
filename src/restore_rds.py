41849d589f68135e22c62695f1b2b5e7270dd86e
freelandr <richard_freeland@hotmail.com>
2022-10-22 13:31:50 +0000

    various

--- src/restore_rds.py
+++ src/restore_rds.py
@@ -1,30 +1,46 @@
 import boto3
 import json
+from operator import itemgetter
+
+rds = None
 
 def lambda_handler(event, context):
     print('restore_rds: START')
-    
+    global rds
     rds = boto3.client('rds')
+    
+    if len(rds.describe_db_instances(DBInstanceIdentifier='kibworth')['DBInstances']) > 0:
+        response_code = 400
+        response_body = {
+            "message": "restore_rds: unable to restore - kibworth database instance already exists",
+            "RDS response": {},
+        }
+    else:
+        rds_response = restore_latest_snapshot()
+        response_code = 200
+        response_body = {
+            "message": "restore_rds: complete",
+            "RDS response": rds_response,
+        }  
+    
+    response = {"statusCode": response_code, "body": json.dumps(response_body, indent=4)}    
+    
+    print(f'restore_rds: END - returing response: {response}')
+    
+    return response        
+   
+def restore_latest_snapshot():
+    global rds
     availableSnapshots=[]
-    for snapshot in rds.describe_db_snapshots(DBInstanceIdentifier='kibworth', 
-                                                SnapshotType='automated')['DBSnapshots']:
+    for snapshot in rds.describe_db_snapshots(DBInstanceIdentifier='kibworth')['DBSnapshots']:
         if snapshot['Status']=='available':
             availableSnapshots.append(snapshot['DBSnapshotIdentifier'])
     
-    latestSnapshot = sorted(availableSnapshots, reverse=True)[0]
+    latestSnapshot = sorted(availableSnapshots, key=itemgetter('SnapshotCreateTime'), reverse=True)[0]
     
     print(f'restoring snapshot {latestSnapshot}')
     
-    rds_response = rds.restore_db_instance_from_db_snapshot(
+    return rds.restore_db_instance_from_db_snapshot(
         DBInstanceIdentifier='kibworth',
         DBSnapshotIdentifier=latestSnapshot
     )
-    
-    response_body = {
-        "message": "restore_rds complete",
-        "RDS response": rds_response,
-    }    
-    
-    response = {"statusCode": 200, "body": json.dumps(response_body)}    
-    
-    return response
